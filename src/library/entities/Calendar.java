20ac495b8de4d6d2504555698974f1ea542703e5
Barry-the-Robot <LoganPoppett@outlook.com>
2022-08-04 22:40:29 +1000

    PR1 #cal1 fixed calendar

--- src/library/entities/Calendar.java
+++ src/library/entities/Calendar.java
@@ -4,63 +4,63 @@ import java.util.concurrent.TimeUnit;
 
 public class Calendar {
 	
-	private static Calendar self;
-	private static java.util.Calendar calendar;
+    private static Calendar self;
+    private static java.util.Calendar calendar;
 	
 	
-	private Calendar() {
-		calendar = java.util.Calendar.getInstance();
-	}
+    private Calendar() {
+	    calendar = java.util.Calendar.getInstance();
+    }
 	
-	public static Calendar getInstance() {
-		if (self == null) {
-			self = new Calendar();
-		}
-		return self;
-	}
+    public static Calendar getInstance() {
+	    if (self == null) {
+		    self = new Calendar();
+	    }
+	    return self;
+    }
 	
-	public void incrementDate(int days) {
-		calendar.add(java.util.Calendar.DATE, days);		
-	}
+    public void incrementDate(int days) {
+	    calendar.add(java.util.Calendar.DATE, days);		
+    }
 	
-	public synchronized void setDate(Date date) {
-		try {
-			calendar.setTime(date);
+    public synchronized void setDate(Date date) {
+	    try {
+		    calendar.setTime(date);
 	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  
 	        calendar.set(java.util.Calendar.MINUTE, 0);  
 	        calendar.set(java.util.Calendar.SECOND, 0);  
 	        calendar.set(java.util.Calendar.MILLISECOND, 0);
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}	
-	}
-	public synchronized Date getDate() {
-		try {
+	    }
+	    catch (Exception e) {
+		    throw new RuntimeException(e);
+	    }	
+    }
+    public synchronized Date getDate() {
+	    try {
 	        calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  
 	        calendar.set(java.util.Calendar.MINUTE, 0);  
 	        calendar.set(java.util.Calendar.SECOND, 0);  
 	        calendar.set(java.util.Calendar.MILLISECOND, 0);
-			return calendar.getTime();
-		}
+		    return calendar.getTime();
+	    }
 		catch (Exception e) {
-			throw new RuntimeException(e);
-		}	
-	}
+		    throw new RuntimeException(e);
+	    }	
+    }
 
-	public synchronized Date getDueDate(int loanPeriod) {
-		Date now = getDate();
-		calendar.add(java.util.Calendar.DATE, loanPeriod);
-		Date dueDate = calendar.getTime();
-		calendar.setTime(now);
-		return dueDate;
-	}
+    public synchronized Date getDueDate(int loanPeriod) {
+	    Date now = getDate();
+	    calendar.add(java.util.Calendar.DATE, loanPeriod);
+	    Date dueDate = calendar.getTime();
+	    calendar.setTime(now);
+	    return dueDate;
+    }
 	
-	public synchronized long getDaysDifference(Date targetDate) {
+    public synchronized long getDaysDifference(Date targetDate) {
 		
-		long Diff_Millis = getDate().getTime() - targetDate.getTime();
-	    long Diff_Days = TimeUnit.DAYS.convert(Diff_Millis, TimeUnit.MILLISECONDS);
-	    return Diff_Days;
-	}
+	    long Diff_Millis = getDate().getTime() - targetDate.getTime();
+	    long diffDays = TimeUnit.DAYS.convert(Diff_Millis, TimeUnit.MILLISECONDS);
+	    return diffDays;
+    }
 
 }
