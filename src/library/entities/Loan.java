c82a3035ea3a7fc0393980ba93c0d7c72fd25a84
genkidesu <tysaunders1@hotmail.com>
2022-08-09 23:49:30 +1000

    removing tabs replacing with spaces

--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -16,16 +16,16 @@ public class Loan implements Serializable {
 
 	
     public Loan(long loanId, Item item, Patron patron, Date dueDate) {
-	this.loanId = loanId;
-	this.item = item;
-	this.patron = patron;
-	this.dueDate = dueDate;
-	this.state = LoanState.CURRENT;
+        this.loanId = loanId;
+        this.item = item;
+        this.patron = patron;
+        this.dueDate = dueDate;
+        this.state = LoanState.CURRENT;
     }
 
     
     public void updateStatus() {
-	if (state == LoanState.CURRENT &&
+        if (state == LoanState.CURRENT &&
             Calendar.getInstance().getDate().after(dueDate)) {
             this.state = LoanState.OVER_DUE;			
 	}
@@ -33,48 +33,48 @@ public class Loan implements Serializable {
 
 	
     public boolean isOverDue() {
-	return state == LoanState.OVER_DUE;
+        return state == LoanState.OVER_DUE;
     }
 
 	
     public Long getId() {
-	return loanId;
+        return loanId;
     }
 
     
     public Date getDueDate() {
-	return dueDate;
+        return dueDate;
     }
 	
 
     public String toString() {
-	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	StringBuilder sb = new StringBuilder();
-	sb.append("Loan:  ").append(loanId).append("\n")
-	  .append("  Borrower ").append(patron.GeT_ID()).append(" : ")
-	  .append(patron.GeT_FiRsT_NaMe()).append(" ").append(patron.GeT_LaSt_NaMe()).append("\n")
-	  .append("  Item ").append(item.getId()).append(" : " )
-	  .append(item.getItemType()).append("\n")
-	  .append(item.getTitle()).append("\n")
-	  .append("  DueDate: ").append(sdf.format(dueDate)).append("\n")
-	  .append("  State: ").append(state);		
-	return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Loan:  ").append(loanId).append("\n")
+          .append("  Borrower ").append(patron.GeT_ID()).append(" : ")
+          .append(patron.GeT_FiRsT_NaMe()).append(" ").append(patron.GeT_LaSt_NaMe()).append("\n")
+          .append("  Item ").append(item.getId()).append(" : " )
+          .append(item.getItemType()).append("\n")
+          .append(item.getTitle()).append("\n")
+          .append("  DueDate: ").append(sdf.format(dueDate)).append("\n")
+          .append("  State: ").append(state);		
+        return sb.toString();
     }
 
 
     public Patron getPatron() {
-	return patron;
+        return patron;
     }
 
 
     public Item getItem() {
-    	return item;
+        return item;
     }
 
 
     public void discharge() {
-	state = LoanState.DISCHARGED;		
+        state = LoanState.DISCHARGED;		
     }
 
 }
