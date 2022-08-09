d8ea74744085bef4657776586e0002e32fda0bd0
Barry-the-Robot <LoganPoppett@outlook.com>
2022-08-09 22:44:22 +1000

    Fixed #pat03

--- src/library/entities/Patron.java
+++ src/library/entities/Patron.java
@@ -8,30 +8,30 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public class Patron implements Serializable {
 
-	private long patronId;
-	private String firstName;
-	private String lastName;
-	private String emailAddress;
-	private long phoneNumber;
-	private double finesOwing;
+    private long patronId;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private long phoneNumber;
+    private double finesOwing;
 	
-	private Map<Long, Loan> currentLoans;
+    private Map<Long, Loan> currentLoans;
 
 	
-	public Patron(String firstName, String lastName, String emailAddress, long phoneNumber, long memberId) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.emailAddress = emailAddress;
-		this.phoneNumber = phoneNumber;
-		this.patronId = memberId;
+    public Patron(String firstName, String lastName, String emailAddress, long phoneNumber, long memberId) {
+	    this.firstName = firstName;
+	    this.lastName = lastName;
+	    this.emailAddress = emailAddress;
+	    this.phoneNumber = phoneNumber;
+	    this.patronId = memberId;
 		
-		this.currentLoans = new HashMap<>();
-	}
+	    this.currentLoans = new HashMap<>();
+    }
 
 	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Patron:  ").append(patronId).append("\n")
+    public String toString() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("Patron:  ").append(patronId).append("\n")
 		  .append("  Name:  ").append(firstName).append(" ").append(lastName).append("\n")
 		  .append("  Email: ").append(emailAddress).append("\n")
 		  .append("  Phone: ").append(phoneNumber)
@@ -39,80 +39,77 @@ public class Patron implements Serializable {
 		  .append(String.format("  Fines Owed :  $%.2f", finesOwing))
 		  .append("\n");
 		
-		for (Loan loan : currentLoans.values()) {
-			sb.append(loan).append("\n");
-		}		  
-		return sb.toString();
-	}
+	    for (Loan loan : currentLoans.values()) {
+		    sb.append(loan).append("\n");
+	    }		  
+	    return sb.toString();
+    }
 
 	
-	public Long getId() {
-		return patronId;
-	}
+    public Long getId() {
+	    return patronId;
+    }
 
 	
-	public List<Loan> getLoans() {
-		return new ArrayList<Loan>(currentLoans.values());
-	}
+    public List<Loan> getLoans() {
+	    return new ArrayList<Loan>(currentLoans.values());
+    }
 
 	
-	public int getNumberOfCurrentLoans() {
-		return currentLoans.size();
-	}
+    public int getNumberOfCurrentLoans() {
+	    return currentLoans.size();
+    }
 
 	
-	public double getFinesOwed() {
-		return finesOwing;
-	}
+    public double getFinesOwed() {
+	    return finesOwing;
+    }
 
 	
-	public void takeOutLoan(Loan loan) {
-		if (!currentLoans.containsKey(loan.getId())) { 
-			currentLoans.put(loan.getId(), loan);
-		}
-		else { 
-			throw new RuntimeException("Duplicate loan added to member");
-		}		
-	}
+    public void takeOutLoan(Loan loan) {
+	    if (!currentLoans.containsKey(loan.getId())) { 
+		    currentLoans.put(loan.getId(), loan);
+	    }
+	    else { 
+		    throw new RuntimeException("Duplicate loan added to member");
+	    }		
+    }
 
-	public void dischargeLoan(Loan loan) {
-		if (currentLoans.containsKey(loan.getId())) { 
-			currentLoans.remove(loan.getId());
-		}
-		else { 
-			throw new RuntimeException("No such loan held by member");
-		}		
-	}
+    public void dischargeLoan(Loan loan) {
+	    if (currentLoans.containsKey(loan.getId())) { 
+		    currentLoans.remove(loan.getId());
+	    }
+	    else { 
+		    throw new RuntimeException("No such loan held by member");
+	    }		
+    }
 	
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+	    return lastName;
+    }
 
 	
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+	    return firstName;
+    }
 
 
-	public void addFine(double fineAmount) {
-		finesOwing += fineAmount;
-	}
+    public void addFine(double fineAmount) {
+	    finesOwing += fineAmount;
+    }
 	
-	public double payFine(double paymentAmount) {
-		if (paymentAmount < 0) {
-			throw new RuntimeException("Member.payFine: amount must be positive");
-		}
-		double change = 0;
-		if (paymentAmount > finesOwing) {
-			change = paymentAmount - finesOwing;
-			finesOwing = 0;
-		}
-		else {
-			finesOwing -= paymentAmount;
-		}
-		return change;
-	}
-
-
-
+    public double payFine(double paymentAmount) {
+	    if (paymentAmount < 0) {
+		    throw new RuntimeException("Member.payFine: amount must be positive");
+	    }
+	    double change = 0;
+	    if (paymentAmount > finesOwing) {
+		    change = paymentAmount - finesOwing;
+		    finesOwing = 0;
+	    }
+	    else {
+		    finesOwing -= paymentAmount;
+	    }
+	    return change;
+    }
 }
