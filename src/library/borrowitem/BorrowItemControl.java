b3badcf770d141952f29f3cf0f0da37655010154
Flyingtreecow <angrybirdnealon@gmail.com>
2022-08-17 11:13:58 +1000

    Solved #bic01 indentation errors

--- src/library/borrowitem/BorrowItemControl.java
+++ src/library/borrowitem/BorrowItemControl.java
@@ -9,117 +9,113 @@ import library.entities.Patron;
 
 public class BorrowItemControl {
 	
-	private BorrowItemUI ui;
+    private BorrowItemUI ui;	
+    private Library Library;
+    private Patron Patron;
+    private enum CONTROL_STATE { initialised, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
+    private CONTROL_STATE state;
 	
-	private Library Library;
-	private Patron Patron;
-	private enum CONTROL_STATE { initialised, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
-	private CONTROL_STATE state;
+    private List<Item> pendingList;
+    private List<Loan> completedList;
+    private Item item;	
 	
-	private List<Item> pendingList;
-	private List<Loan> completedList;
-	private Item item;
-	
-	
-	public BorrowItemControl() {
-		this.Library = Library.getInstance();
-		state = CONTROL_STATE.initialised;
-	}
+    public BorrowItemControl() {
+        this.Library = Library.getInstance();
+        state = CONTROL_STATE.initialised;
+    }
 	
 
-	public void setUI(BorrowItemUI ui) {
-		if (!state.equals(CONTROL_STATE.initialised)) 
-			throw new RuntimeException("BorrowItemControl: cannot call setUI except in INITIALISED state");
+    public void setUI(BorrowItemUI ui) {
+        if (!state.equals(CONTROL_STATE.initialised)) 
+	   throw new RuntimeException("BorrowItemControl: cannot call setUI except in INITIALISED state");
 			
-		this.ui = ui;
-		ui.setReady();
-		state = CONTROL_STATE.READY;		
-	}
+        this.ui = ui;
+        ui.setReady();
+        state = CONTROL_STATE.READY;		
+    }
 
 		
-	public void cardSwiped(long patronId) {
-		if (!state.equals(CONTROL_STATE.READY)) 
-			throw new RuntimeException("BorrowItemControl: cannot call cardSwiped except in READY state");
+    public void cardSwiped(long patronId) {
+        if (!state.equals(CONTROL_STATE.READY)) 
+	   throw new RuntimeException("BorrowItemControl: cannot call cardSwiped except in READY state");
 			
-		Patron = Library.getPatron(patronId);
-		if (Patron == null) {
-			ui.display("Invalid patronId");
-			return;
-		}
-		if (Library.canPatronBorrow(Patron)) {
-			pendingList = new ArrayList<>();
-			ui.setScanning();
-			state = CONTROL_STATE.SCANNING; 
-		}
-		else {
-			ui.display("Patron cannot borrow at this time");
-			ui.setRestricted(); 
-		}
-	}
+        Patron = Library.getPatron(patronId);
+        if (Patron == null) {
+            ui.display("Invalid patronId");
+            return;
+        }
+        if (Library.canPatronBorrow(Patron)) {
+            pendingList = new ArrayList<>();
+            ui.setScanning();
+            state = CONTROL_STATE.SCANNING; 
+        }
+        else {
+            ui.display("Patron cannot borrow at this time");
+            ui.setRestricted(); 
+        }
+    }
 	
 	
-	public void itemScanned(int itemId) {
-		item = null;
-		if (!state.equals(CONTROL_STATE.SCANNING)) 
-			throw new RuntimeException("BorrowItemControl: cannot call itemScanned except in SCANNING state");
+    public void itemScanned(int itemId) {
+        item = null;
+        if (!state.equals(CONTROL_STATE.SCANNING)) 
+            throw new RuntimeException("BorrowItemControl: cannot call itemScanned except in SCANNING state");
 			
-		item = Library.getItem(itemId);
-		if (item == null) {
-			ui.display("Invalid itemId");
-			return;
-		}
-		if (!item.isAvailable()) {
-			ui.display("Item cannot be borrowed");
-			return;
-		}
-		pendingList.add(item);
-		for (Item item : pendingList) 
-			ui.display(item);
+        item = Library.getItem(itemId);
+        if (item == null) {
+            ui.display("Invalid itemId");
+            return;
+        }
+        if (!item.isAvailable()) {
+            ui.display("Item cannot be borrowed");
+            return;
+        }
+        pendingList.add(item);
+        for (Item item : pendingList) 
+             ui.display(item);
 		
-		if (Library.getNumberOfLoansRemainingForPatron(Patron) - pendingList.size() == 0) {
-			ui.display("Loan limit reached");
-			borrowingCompleted();
-		}
-	}
+        if (Library.getNumberOfLoansRemainingForPatron(Patron) - pendingList.size() == 0) {
+            ui.display("Loan limit reached");
+            borrowingCompleted();
+        }
+    }
 	
 	
-	public void borrowingCompleted() {
-		if (pendingList.size() == 0) 
-			cancel();
+    public void borrowingCompleted() {
+        if (pendingList.size() == 0) 
+            cancel();
 		
-		else {
-			ui.display("\nFinal Borrowing List");
-			for (Item item : pendingList) 
-				ui.display(item);
+        else {
+            ui.display("\nFinal Borrowing List");
+            for (Item item : pendingList) 
+                ui.display(item);
 			
-			completedList = new ArrayList<Loan>();
-			ui.setFinalising();
-			state = CONTROL_STATE.FINALISING;
-		}
+            completedList = new ArrayList<Loan>();
+            ui.setFinalising();
+            state = CONTROL_STATE.FINALISING;
 	}
+    }
 
 
-	public void commitLoans() {
-		if (!state.equals(CONTROL_STATE.FINALISING)) 
-			throw new RuntimeException("BorrowItemControl: cannot call commitLoans except in FINALISING state");
+    public void commitLoans() {
+        if (!state.equals(CONTROL_STATE.FINALISING)) 
+            throw new RuntimeException("BorrowItemControl: cannot call commitLoans except in FINALISING state");
 			
-		for (Item B : pendingList) {
-			Loan loan = Library.issueLoan(B, Patron);
-			completedList.add(loan);			
-		}
-		ui.display("Completed Loan Slip");
-		for (Loan loan : completedList) 
-			ui.display(loan);
+        for (Item B : pendingList) {
+            Loan loan = Library.issueLoan(B, Patron);
+            completedList.add(loan);			
+        }
+        ui.display("Completed Loan Slip");
+        for (Loan loan : completedList) 
+            ui.display(loan);
 		
-		ui.setCompleted();
-		state = CONTROL_STATE.COMPLETED;
-	}
+        ui.setCompleted();
+        state = CONTROL_STATE.COMPLETED;
+    }
 
 	
-	public void cancel() {
-		ui.setCancelled();
-		state = CONTROL_STATE.CANCELLED;
-	}
-	
-	
+    public void cancel() {
+        ui.setCancelled();
+        state = CONTROL_STATE.CANCELLED;
+    }	
 }
