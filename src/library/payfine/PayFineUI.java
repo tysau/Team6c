ad668ad2126f50f373da95451f39def0eee50b93
Barry-the-Robot <LoganPoppett@outlook.com>
2022-08-18 17:04:47 +1000

    PayFineUI indentation fixing

--- src/library/payfine/PayFineUI.java
+++ src/library/payfine/PayFineUI.java
@@ -5,117 +5,112 @@ import java.util.Scanner;
 public class PayFineUI {
 
 
-	private enum uI_sTaTe { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
+    private enum uI_sTaTe { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
 
-	private PayFineControl CoNtRoL;
-	private Scanner ScAnNeR;
-	private uI_sTaTe StAtE;
+    private PayFineControl CoNtRoL;
+    private Scanner ScAnNeR;
+    private uI_sTaTe StAtE;
 
 	
-	public PayFineUI(PayFineControl control) {
-		this.CoNtRoL = control;
-		ScAnNeR = new Scanner(System.in);
-		StAtE = uI_sTaTe.INITIALISED;
-		control.setUI(this);
-	}
+    public PayFineUI(PayFineControl control) {
+        this.CoNtRoL = control;
+        ScAnNeR = new Scanner(System.in);
+        StAtE = uI_sTaTe.INITIALISED;
+        control.setUI(this);
+    }
 	
 	
-
-	public void RuN() {
-		DiSpLaYoUtPuT("Pay Fine Use Case UI\n");
+    public void RuN() {
+        DiSpLaYoUtPuT("Pay Fine Use Case UI\n");
 		
-		while (true) {
+        while (true) {
 			
-			switch (StAtE) {
+            switch (StAtE) {
 			
-			case READY:
-				String PaT_Str = GeTiNpUt("Swipe patron card (press <enter> to cancel): ");
-				if (PaT_Str.length() == 0) {
-					CoNtRoL.cancel();
-					break;
-				}
-				try {
-					long PAtroN_ID = Long.valueOf(PaT_Str).longValue();
-					CoNtRoL.cardSwiped(PAtroN_ID);
-				}
-				catch (NumberFormatException e) {
-					DiSpLaYoUtPuT("Invalid patronID");
-				}
-				break;
+            case READY:
+                String PaT_Str = GeTiNpUt("Swipe patron card (press <enter> to cancel): ");
+                if (PaT_Str.length() == 0) {
+                    CoNtRoL.cancel();
+                    break;
+                }
+                try {
+                    long PAtroN_ID = Long.valueOf(PaT_Str).longValue();
+                    CoNtRoL.cardSwiped(PAtroN_ID);
+                }
+                catch (NumberFormatException e) {
+                    DiSpLaYoUtPuT("Invalid patronID");
+                }
+                break;
 				
-			case PAYING:
-				double AmouNT = 0;
-				String Amt_Str = GeTiNpUt("Enter amount (<Enter> cancels) : ");
-				if (Amt_Str.length() == 0) {
-					CoNtRoL.cancel();
-					break;
-				}
-				try {
-					AmouNT = Double.valueOf(Amt_Str).doubleValue();
-				}
-				catch (NumberFormatException e) {}
-				if (AmouNT <= 0) {
-					DiSpLaYoUtPuT("Amount must be positive");
-					break;
-				}
-				CoNtRoL.payFine(AmouNT);
-				break;
+            case PAYING:
+                double AmouNT = 0;
+                String Amt_Str = GeTiNpUt("Enter amount (<Enter> cancels) : ");
+                if (Amt_Str.length() == 0) {
+                    CoNtRoL.cancel();
+                    break;
+                }
+                try {
+                    AmouNT = Double.valueOf(Amt_Str).doubleValue();
+                }
+                catch (NumberFormatException e) {}
+                if (AmouNT <= 0) {
+                    DiSpLaYoUtPuT("Amount must be positive");
+                    break;
+                }
+                CoNtRoL.payFine(AmouNT);
+                break;
 								
-			case CANCELLED:
-				DiSpLaYoUtPuT("Pay Fine process cancelled");
-				return;
+            case CANCELLED:
+                DiSpLaYoUtPuT("Pay Fine process cancelled");
+                return;
 			
-			case COMPLETED:
-				DiSpLaYoUtPuT("Pay Fine process complete");
-				return;
+            case COMPLETED:
+                DiSpLaYoUtPuT("Pay Fine process complete");
+                return;
 			
-			default:
-				DiSpLaYoUtPuT("Unhandled state");
-				throw new RuntimeException("FixBookUI : unhandled state :" + StAtE);			
+            default:
+                DiSpLaYoUtPuT("Unhandled state");
+                throw new RuntimeException("FixBookUI : unhandled state :" + StAtE);			
 			
-			}		
-		}		
-	}
+            }		
+        }		
+    }
 
 	
-	private String GeTiNpUt(String prompt) {
-		System.out.print(prompt);
-		return ScAnNeR.nextLine();
-	}	
+    private String GeTiNpUt(String prompt) {
+        System.out.print(prompt);
+        return ScAnNeR.nextLine();
+    }	
 		
 		
-	private void DiSpLaYoUtPuT(Object object) {
-		System.out.println(object);
-	}	
+    private void DiSpLaYoUtPuT(Object object) {
+        System.out.println(object);
+    }	
 			
 
-	public void DiSplAY(Object object) {
-		DiSpLaYoUtPuT(object);
-	}
+    public void DiSplAY(Object object) {
+        DiSpLaYoUtPuT(object);
+    }
 
 
-	public void SeTcOmPlEtEd() {
-		StAtE = uI_sTaTe.COMPLETED;
-		
-	}
+    public void SeTcOmPlEtEd() {
+        StAtE = uI_sTaTe.COMPLETED;		
+    }
 
 
-	public void SeTpAyInG() {
-		StAtE = uI_sTaTe.PAYING;
-		
-	}
+    public void SeTpAyInG() {
+        StAtE = uI_sTaTe.PAYING;
+    }
 
 
-	public void SeTcAnCeLlEd() {
-		StAtE = uI_sTaTe.CANCELLED;
-		
-	}
+    public void SeTcAnCeLlEd() {
+        StAtE = uI_sTaTe.CANCELLED;	
+    }
 
 
-	public void setReady() {
-		StAtE = uI_sTaTe.READY;
-		
-	}
+    public void setReady() {
+        StAtE = uI_sTaTe.READY;	
+    }
 
 
 }
